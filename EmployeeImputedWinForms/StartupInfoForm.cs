f323d847606aa7934f89c1df91f9d84ec07a8c12
Christopher Lane <cslane@gmail.com>
2026-08-28 08:39:47 -0400

    Corrected form size and button position

--- EmployeeImputedWinForms/StartupInfoForm.cs
+++ EmployeeImputedWinForms/StartupInfoForm.cs
@@ -16,6 +16,14 @@ namespace EmployeeImputedWinForms
         public StartupInfoForm()
         {
             InitializeComponent();
+
+            // Optional: Set the form to a percentage of the screen size
+            var screen = Screen.FromControl(this);
+            int width = (int)(screen.WorkingArea.Width * 0.8);   // 80% of screen width
+            int height = (int)(screen.WorkingArea.Height * 0.8); // 80% of screen height
+
+            this.Size = new System.Drawing.Size(width, height);
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void BtnContinue_Click(object sender, EventArgs e)
@@ -38,6 +46,8 @@ namespace EmployeeImputedWinForms
             lblInfo = new Label();
             btnContinue = new Button();
             btnExit = new Button();
+            Panel buttonPanel = new Panel();
+
             SuspendLayout();
             // 
             // lblHeader
@@ -48,7 +58,7 @@ namespace EmployeeImputedWinForms
             lblHeader.Name = "lblHeader";
             lblHeader.Size = new Size(2400, 100);
             lblHeader.TabIndex = 0;
-            lblHeader.Text = "Please review this information before continuing.";
+            lblHeader.Text = "Please review the information below.";
             lblHeader.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // lblInfo
@@ -57,24 +67,33 @@ namespace EmployeeImputedWinForms
             lblInfo.Font = new Font("Segoe UI", 10F);
             lblInfo.Location = new Point(150, 100);
             lblInfo.Name = "lblInfo";
-            lblInfo.Size = new Size(2115, 1348);
+            lblInfo.AutoSize = false;
+            lblInfo.Dock = DockStyle.Fill;
             lblInfo.TabIndex = 1;
             lblInfo.Text = resources.GetString("lblInfo.Text");
+            lblInfo.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // buttonPanel
+            // 
+            buttonPanel.Dock = DockStyle.Bottom;
+            buttonPanel.Height = 75;
+            buttonPanel.Padding = new Padding(20,10,20,10);
+            buttonPanel.Name = "buttonPanel";
             // 
             // btnContinue
             // 
-            btnContinue.Location = new Point(150, 1495);
+            btnContinue.Dock = DockStyle.Left;
             btnContinue.Name = "btnContinue";
-            btnContinue.Size = new Size(400, 80);
+            btnContinue.Width = 150;
             btnContinue.TabIndex = 2;
             btnContinue.Text = "Continue";
             btnContinue.Click += BtnContinue_Click;
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(1865, 1495);
+            btnExit.Dock = DockStyle.Right;
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(400, 80);
+            btnExit.Width = 150;
             btnExit.TabIndex = 3;
             btnExit.Text = "Exit";
             btnExit.Click += BtnExit_Click;
@@ -84,10 +103,11 @@ namespace EmployeeImputedWinForms
             AcceptButton = btnContinue;
             CancelButton = btnExit;
             ClientSize = new Size(2400, 1600);
-            Controls.Add(lblHeader);
             Controls.Add(lblInfo);
-            Controls.Add(btnContinue);
-            Controls.Add(btnExit);
+            Controls.Add(buttonPanel);
+            Controls.Add(lblHeader);
+            buttonPanel.Controls.Add(btnContinue);
+            buttonPanel.Controls.Add(btnExit);
             Name = "StartupInfoForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Employee Imputed Calculator Info Form";
@@ -95,4 +115,3 @@ namespace EmployeeImputedWinForms
         }
     }
 }
-
